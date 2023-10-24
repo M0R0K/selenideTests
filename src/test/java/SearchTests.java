629fa0bbefdf721bf17ddc824219c538edd3c6d6
Konstantin Ponomarenko <shemiakin1994@yandex.ru>
2023-10-24 20:00:03 +0300

    add search test

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -14,11 +14,10 @@ public class SearchTests {
     }
 
 
-
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+        $("[id=search]").shouldHave(text("https://selenide.org"));
     }
 }
