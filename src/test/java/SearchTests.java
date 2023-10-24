68f8c0df8bc7f3164456963cc72574a816f38579
M0R0K <shemiakin1994@yandex.ru>
2023-10-24 19:03:22 +0300

    add search test

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,24 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import com.codeborne.selenide.Configuration;
+public class SearchTests {
+
+    @BeforeEach
+    public void init(){
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+
+
+    @Test
+    void successfulSearchTest() {
+        open("https://www.google.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=search]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
